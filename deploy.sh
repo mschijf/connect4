691cb801253dd587277d49fcc587911672fadd3c
Martin Schijf <mschijf@bol.com>
2021-09-20 17:27:50 +0200

    generalized deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-program="connect4"
-cd /Users/mschijf/PriveSources/$program
-mvn clean package 
-ssh pi rm $program/$program*.jar
-sftp pi <<< $'mput target/'$program'*.jar '$program'/'
-ssh pi ./$program/start.sh
-
